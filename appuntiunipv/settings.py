b9758d77c21f88a387bded17619a8fe2b1b61605
federicospataro <51948011+federicospataro@users.noreply.github.com>
2022-09-23 21:38:32 +0200

    fix10
    fix10

--- appuntiunipv/settings.py
+++ appuntiunipv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(m-jlqf9nq3$6j_nt5($gwr8!+_myj1v16_*nxv+-trasve_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SECURE_SSL_REDIRECT = True
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
