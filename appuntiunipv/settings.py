95ec050cfbaebb3fc2a608c3eb0786d9244b12c9
federicospataro <51948011+federicospataro@users.noreply.github.com>
2022-09-23 20:57:19 +0200

    fix8
    fix8

--- appuntiunipv/settings.py
+++ appuntiunipv/settings.py
@@ -125,8 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    '/staticfiles/'
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Default primary key field type
